c20dc2203a3ee434106807f7473ec4db98a38ece
Sendiko <91923470+Sendiko@users.noreply.github.com>
2021-10-18 10:28:05 +0700

    Add files via upload

---
+++ 3_XI RPL_6.php
@@ -0,0 +1,16 @@
+<?php 
+
+    $nama = array ('Satria', 'Arsyi', 'Dhika', 'Evan', 'Fabian');
+    $kelas = array ('Rpl 1', 'Rpl 2', 'Rpl 3', 'Rpl 4', 'Rpl 5');
+
+    echo 'Nama = ' . $nama[0] . ', Kelas = ' . $kelas[0];
+    echo "<br>";
+    echo 'Nama = ' . $nama[1] . ', Kelas = ' . $kelas[1];
+    echo "<br>";
+    echo 'Nama = ' . $nama[2] . ', Kelas = ' . $kelas[2];
+    echo "<br>";
+    echo 'Nama = ' . $nama[3] . ', Kelas = ' . $kelas[3];
+    echo "<br>";
+    echo 'Nama = ' . $nama[4] . ', Kelas = ' . $kelas[4];
+
+?>
